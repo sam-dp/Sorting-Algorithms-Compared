b1354c6ec9a15bfa18d88d6ed3918a412b072bc0
sam-dp <67991792+sam-dp@users.noreply.github.com>
2023-02-08 01:53:18 -0800

    commit

--- src/main.java
+++ src/main.java
@@ -1,62 +1,74 @@
 import java.util.*;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 class main {
 
-    // public static void createGUI() {
-
-    //     JPanel controlWindow = new JPanel();
-    //     controlWindow.setBackground(Color.red);
-    //     controlWindow.setPreferredSize(new Dimension(250, 720));
-
-    //     JPanel sortWindow = new JPanel();
-    //     sortWindow.setBackground(Color.BLACK);
-    //     sortWindow.setBorder(new EmptyBorder(10, 10, 10, 10));
-
-    //     JFrame myFrame = new JFrame();
-    //     myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    //     myFrame.setSize(1280, 720);
-    //     myFrame.setLayout(new BorderLayout());
-    //     myFrame.add(controlWindow, BorderLayout.WEST);
-    //     myFrame.add(sortWindow, BorderLayout.CENTER);
-
-    //     myFrame.setResizable(false);
-    //     myFrame.setTitle("Visualized Sorting Algorithms");
-
-
-
-    //     //ImageIcon image = new ImageIcon("SOURCE TITLE");
-    //     //this.setIconImage(image.getImage());
-    //     myFrame.setVisible(true);
-    // }
-
+    public int arraySize;
+    public float[] array = new float[arraySize];
 
     public static void main (String[] args) {
         
     }
 
-    // public static void computer() {
-    //     JFrame frame = new JFrame("Swing Example");
-    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    //     frame.setSize(800, 600);
-
-    //     JPanel leftPanel = new JPanel();
-    //     leftPanel.setPreferredSize(new Dimension(200, 0));
-    //     leftPanel.setBackground(Color.LIGHT_GRAY);
-    //     leftPanel.setBorder(BorderFactory.createLineBorder(Color.GREEN, 2));
+    public float[] shuffle(float[] array)  {
+        return array;
         
-    //     JPanel rightPanel = new JPanel();
-    //     rightPanel.setBackground(Color.WHITE);
-    //     rightPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.GREEN, 2), 
-    //                          BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+    }
 
-    //     frame.getContentPane().add(leftPanel, BorderLayout.WEST);
-    //     frame.getContentPane().add(rightPanel, BorderLayout.CENTER);
+    public void drawArray(float[] array) {
 
-    //     frame.setVisible(true);
+    }
 
 
-    // }
+    //////////////////
+    // SORT METHODS //
+    //////////////////
+
+    public float[] binarySort(float[] array)  {
+        
+        return array;
+        
+    }
+
+    public float[] bubbleSort(float[] array)  {
+        int arrayLength = array.length;
+        for (int i = 0; i < arrayLength - 1; i++) {
+            for (int j = 0; j < arrayLength - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    float temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+        
+    }
+
+    public float[] mergeSort(float[] array)  {
+        return array;
+        
+    }
+
+    public float[] quickSort(float[] array)  {
+        return array;
+        
+    }
+
+    public float[] selectionSort(float[] array)  {
+        return array;
+        
+    }
+
+    public float[] sequentialSort(float[] array)  {
+        return array;
+        
+    }
+
+
+    
+
+    
 }
