7d0114a198f0e362de1744ea6a0f2f56e818fd91
sam-dp <67991792+sam-dp@users.noreply.github.com>
2023-02-07 23:40:38 -0800

    chaos

--- src/main.java
+++ src/main.java
@@ -1,11 +1,62 @@
 import java.util.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 class main {
 
+    // public static void createGUI() {
+
+    //     JPanel controlWindow = new JPanel();
+    //     controlWindow.setBackground(Color.red);
+    //     controlWindow.setPreferredSize(new Dimension(250, 720));
+
+    //     JPanel sortWindow = new JPanel();
+    //     sortWindow.setBackground(Color.BLACK);
+    //     sortWindow.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+    //     JFrame myFrame = new JFrame();
+    //     myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     myFrame.setSize(1280, 720);
+    //     myFrame.setLayout(new BorderLayout());
+    //     myFrame.add(controlWindow, BorderLayout.WEST);
+    //     myFrame.add(sortWindow, BorderLayout.CENTER);
+
+    //     myFrame.setResizable(false);
+    //     myFrame.setTitle("Visualized Sorting Algorithms");
+
+
+
+    //     //ImageIcon image = new ImageIcon("SOURCE TITLE");
+    //     //this.setIconImage(image.getImage());
+    //     myFrame.setVisible(true);
+    // }
+
 
     public static void main (String[] args) {
-        MyFrame myFrame = new MyFrame();
+        
     }
+
+    // public static void computer() {
+    //     JFrame frame = new JFrame("Swing Example");
+    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     frame.setSize(800, 600);
+
+    //     JPanel leftPanel = new JPanel();
+    //     leftPanel.setPreferredSize(new Dimension(200, 0));
+    //     leftPanel.setBackground(Color.LIGHT_GRAY);
+    //     leftPanel.setBorder(BorderFactory.createLineBorder(Color.GREEN, 2));
+        
+    //     JPanel rightPanel = new JPanel();
+    //     rightPanel.setBackground(Color.WHITE);
+    //     rightPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.GREEN, 2), 
+    //                          BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+
+    //     frame.getContentPane().add(leftPanel, BorderLayout.WEST);
+    //     frame.getContentPane().add(rightPanel, BorderLayout.CENTER);
+
+    //     frame.setVisible(true);
+
+
+    // }
 }
